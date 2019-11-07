9825e865b50c95c06420cc486be396506aab20f5
enikansaye mayowa <enikansaye13@gmail.com>
2019-11-08 01:29:25 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import TodoInput from './component/TodoInput';
+import TodoList from "./component/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import "bootstrap/dist/css/bootstrap.min.css";
+//import uuid from "uuid";
+
+class App extends Component {
+  state = {
+    items:[],
+    id: 1,
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    console.log(e.target, '...value')
+    this.setState({
+      item: e.target.value
+    });
+    
+  };
+    handleSubmit = (e) =>{
+e.preventDefault();
+console.log(this.state.item, '...item')
+const newItem={
+  id:this.state.id,
+  title:this.state.item
+};
+console.log(newItem)
+  const updatedItems =[...this.state.items,newItem];
+  this.setState({
+    items:updatedItems,
+    item:'',
+    id: 0,
+    editItem:false
+  });
+    };
+  
+  render() {
+   return (
+     <div className="container">
+       <div className="row">
+         <div className="col-10 mx-auto col-md-8 mt-4">
+           <h3 className="text-capitalize  text-center">todo input</h3>
+           <TodoInput 
+           item={this.state.item}
+           handlechange={this.handleChange}
+           handleSubmit={this.handleSubmit}/>
+           <TodoList items={this.state.items}/>
+      </div>
+       </div>
+     </div>
+      
   );
+}
 }
 
 export default App;
